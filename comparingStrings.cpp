416bc2193f7343a51adc1530b7a4570de9dbcf73
Sanjeet Agrawal <55800083+Sanjeet09@users.noreply.github.com>
2022-06-28 21:52:35 +0530

    Create comparingStrings.cpp

---
+++ comparingStrings.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    string s1="I love programming";
+    string s2="I love programming";
+    string s3="I programming";
+
+    cout<<s1.compare(s2)<<endl;
+    cout<<s1.compare(s3)<<endl;
+
+    return 0;
+}
